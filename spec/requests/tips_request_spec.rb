a58895f94630ae0f3b917bf9b6d5bc15fb5b4b8e
Jordan Sewell <jrsewell400@gmail.com>
2020-07-28 10:28:31 -0600

    Finish tip request test

--- spec/requests/tips_request_spec.rb
+++ spec/requests/tips_request_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 describe 'Tips API' do
   it 'sends a tip' do
     tip1 = Tip.create!(description: "Turn it off. Energy conservation is one of the most important things you can do to reduce your carbon footprint.")
-    tip2 = Tip.create!(description: "Turn it off. Energy conservation is one of the most important things you can do to reduce your carbon footprint.")
+    tip2 = Tip.create!(description: "Electronics that are plugged in still consume energy.  Unplug unused electronics to save money on your energy bill!")
+    tips = [tip1.description, tip2.description]
 
-    get '/api/vi/tips'
+    get '/api/v1/tips'
 
     expect(response).to be_successful
 
     parsed = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
-    # expect(parsed.description)
+    
+    expect(tips).to include(parsed[:description])
   end
 end
