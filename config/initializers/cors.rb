6a1d9db70611495101cf9c8d35818f3d0e6bf855
Alex Latham <alexlthm@gmail.com>
2020-07-28 12:51:29 -0600

    Allow CORS on all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://secret-meadow-99085.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
