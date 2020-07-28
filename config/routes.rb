051faab6013788398a49d66da1834bda04130aa7
Jordan Sewell <jrsewell400@gmail.com>
2020-07-28 10:25:14 -0600

    Remove tip namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :tips do
-        get '/tips', to: 'tips#show'
-      end
+      get '/tips', to: 'tips#show'
     end
   end
 end
