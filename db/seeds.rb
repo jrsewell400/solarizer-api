20639de0a7b38056bdf1b0ee301f6c9efd5688c7
Jordan Sewell <jrsewell400@gmail.com>
2020-07-28 10:26:25 -0600

    Create seeds for tips

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tip.destroy_all
+
+Tip.create(description: "Turn it off. Energy conservation is one of the most important things you can do to reduce your carbon footprint.")
+Tip.create(description: "Collect plastic and recycle film. Many grocery stores have recycling drop-offs for this less-commonly recycled type of plastic.")
+Tip.create(description: "Use your microwave. Honestly, you’d be surprised by how much more energy efficient microwaves are compared to conventional ovens.")
+Tip.create(description: "Buy local. The closer to home products are made and bought, the less carbon is created with their transportation.")
+Tip.create(description: "Try to cut out plastic, especially types which cannot be recycled locally.")
+Tip.create(description: "Electronics that are plugged in still consume energy.  Unplug unused electronics to save money on your energy bill!")
+Tip.create(description: "Eat less meat!  Meat is one of the biggest contributors to climate change.")
